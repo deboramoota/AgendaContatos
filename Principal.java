178b1a8c6c56b9b42c96b89bfe7694b62a2b7908
Débora <debora.ribeiro63@aluno.ifce.edu.br>
2026-09-21 08:33:09 -0300

    Adicionando a V.1.1.1 da Agenda de Contatos

--- Principal.java
+++ Principal.java
@@ -1,44 +1,50 @@
 package br.edu.principal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
+public class Principal {
 
+    public static void main(String[] args) {
 
-public class Uteis {
-	public static void mostraInicializacao() {
-    	System.out.println("==========================");
-        System.out.println("     AGENDA DE CONTATOS    ");
-        System.out.println("          v1.1.0           ");
-        System.out.println("==========================");
-        System.out.println("Bem-vindo!");
-    }
-    
-    public static void mostraMenu() {
-    	System.out.println();
-        System.out.println("1 - Adicionar contato");
-        System.out.println("2 - Listar contatos");
-        System.out.println("3 - Procurar contato");
-        System.out.println("4 - Alterar contato");
-        System.out.println("5 - Excluir contato");
-        System.out.println("6 - Sair");
-        System.out.println("7 - Informações Sobre a Agenda de Contatos");
-    }
-    
-    public static int selecionaOpcao(Scanner sc) {
-    	System.out.print("Escolha uma opção: ");
-        int opc = sc.nextInt();
-        sc.nextLine();
-        return opc;
-    }
+        List<String> nomes = new ArrayList<>();
+        List<String> celulares = new ArrayList<>();
+        List<String> emails = new ArrayList<>();
 
-    public static void sair(boolean continuar) {
-    	System.out.println("Saindo da Agenda de Contatos...");
-        continuar = false;
-    }
-    
-    public static void sobre() {
-    	JOptionPane.showMessageDialog(null, 
-    			"Desenvolvido por Débora!");
+        int opcao;
+
+        Scanner sc = new Scanner(System.in);
+
+        Uteis.mostraInicializacao();
+
+        while (true) {
+
+            Uteis.mostraMenu();
+
+            opcao = Uteis.selecionaOpcao(sc);
+
+            switch (opcao) {
+
+                case 1 -> Agenda.adicionar(sc, nomes, celulares, emails);
+
+                case 2 -> Agenda.listar(nomes, celulares, emails);
+
+                case 3 -> Agenda.pesquisar(sc, nomes, celulares, emails);
+
+                case 4 -> Agenda.atualizar(sc, nomes, celulares, emails);
+
+                case 5 -> Agenda.excluir(sc, nomes, celulares, emails);
+
+                case 6 -> {
+                    System.out.println("Saindo da Agenda de Contatos...");
+                    return;
+                }
+
+                case 7 -> Uteis.sobre();
+
+                default -> System.out.println("Opção inválida!");
+            }
+        }
     }
 }
